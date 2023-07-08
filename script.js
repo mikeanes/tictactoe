7be4673a723a8bdee8c06d0b8006c1c68fc558de
Michael <m.anestopoulos24@gmail.com>
2023-07-08 14:22:55 -0400

    added buttons in html

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+const Player = (number, sign) => {
+    return{number, sign};
+};
+
 //Make gameboard empty array and winning conditions.
 //have method that analyses gameboard to see if any
 //winning conditions are met by filtering through
 //the board and only returning the array with number
 //of position. 
+
+//Also in this module you'll want to implement a function
+//that places the players sign into the board array and probably
+//one to reset the board but thats for later
 const gameBoard = (() => {
     const board = 
-    ['o','x','o',
-     'x','o','o',
-     'x','x','o'];
+    ['','','o',
+     'x','o','',
+     'x','x','x'];
     
     const winningConditions = 
     [[0, 1, 2],
@@ -28,18 +36,23 @@ const gameBoard = (() => {
         return winningConditions.some(condition => 
             condition.every(index => placements.includes(index)));
    }
-console.log(checkWin('o'));
+console.log(checkWin('x'));
 })();
 
 //This module will take care of creating the players
 //and alternating between them. It will access the gameboard
 //methods in order to update the board array
 const gameController = (() => {
+    const playerOne = Player(1, 'x');
+    const playerTwo = Player(2, 'o');
+
 
 })();
 
-const Player = (number, sign) => {
-    return{number, sign};
-};
+//This module will update the html
+const displayController = (() => {
+
+})();
+
 
 
