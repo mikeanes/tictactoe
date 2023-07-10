a68f50abe4d664476c81819fd342fe7273023d7d
Michael <m.anestopoulos24@gmail.com>
2023-07-10 13:57:51 -0400

    fixed current player sign updating in dom

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ const Player = (number, sign) => {
 //one to reset the board but thats for later
 const gameBoard = (() => {
     const board = 
-    ['','','o',
-     'x','o','',
-     'x','x','x'];
+    ['','','',
+     '','','',
+     '','',''];
     
     const winningConditions = 
     [[0, 1, 2],
@@ -27,6 +27,12 @@ const gameBoard = (() => {
     [0, 4, 8],
     [2, 4, 6]];
 
+    const placeSign = (index, playerSign) => {
+        if (board[index] === ''){
+            board[index] = playerSign;
+        }
+    };
+
    const checkWin = (playerSign) => {
         const placements = board.map((element, index) => {
             if (element === playerSign){
@@ -35,23 +41,52 @@ const gameBoard = (() => {
         }).filter(index => index !== undefined);
         return winningConditions.some(condition => 
             condition.every(index => placements.includes(index)));
-   }
-console.log(checkWin('x'));
+   };
+
+   return{placeSign, checkWin};
 })();
 
 //This module will take care of creating the players
 //and alternating between them. It will access the gameboard
-//methods in order to update the board array
+//methods in order to update the board array.
+
 const gameController = (() => {
     const playerOne = Player(1, 'x');
     const playerTwo = Player(2, 'o');
 
+    let currentPlayer = playerOne;
 
+    const changeTurn = () => {
+        if(currentPlayer === playerOne){
+            currentPlayer = playerTwo;
+        }else{
+            currentPlayer = playerOne;
+        }
+    };
+
+    const playTurn = () => {
+        changeTurn();
+        console.log(currentPlayer.number);
+    };
+
+    const getCurrentPlayerSign = () => {
+        return currentPlayer.sign;
+    }
+
+   
+    return{getCurrentPlayerSign, playTurn};
 })();
 
 //This module will update the html
 const displayController = (() => {
+    const squares = document.querySelectorAll('.gameBoard button');
 
+    squares.forEach(square => {
+        square.addEventListener('click', () => {
+            square.textContent = gameController.getCurrentPlayerSign();
+            gameController.playTurn();
+        });
+    });
 })();
 
 
